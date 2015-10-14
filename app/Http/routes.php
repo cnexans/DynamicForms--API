6672ddb36f9f8d02aecc9fdeebfe69f7e7caac3e
Carlos David Nexans <carlmycol@gmail.com>
2015-10-14 12:27:45 -0430

    route oauth/token works fine

---
+++ app/Http/routes.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+App::singleton('oauth2', function() {
+	
+	$storage = new OAuth2\Storage\Pdo
+	([
+		'dsn' => 'mysql:dbname='.env('DB_DATABASE').';host=' . env('DB_HOST'),
+		'username' => env('DB_USERNAME'),
+		'password' => env('DB_PASSWORD')
+	]);
+
+	$server = new OAuth2\Server($storage);
+	
+	$server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));
+	$server->addGrantType(new OAuth2\GrantType\UserCredentials($storage));
+	
+	return $server;
+});
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+
+
+Route::post('oauth/token', function()
+{
+	$bridgedRequest  = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());
+	$bridgedResponse = new OAuth2\HttpFoundationBridge\Response();
+	
+	$bridgedResponse = App::make('oauth2')->handleTokenRequest($bridgedRequest, $bridgedResponse);
+	
+	return $bridgedResponse;
+});
+
+
+Route::get('private', function()
+{
+	$bridgedRequest  = OAuth2\HttpFoundationBridge\Request::createFromRequest(Request::instance());
+	$bridgedResponse = new OAuth2\HttpFoundationBridge\Response();
+	
+	if (App::make('oauth2')->verifyResourceRequest($bridgedRequest, $bridgedResponse)) {
+		
+		$token = App::make('oauth2')->getAccessTokenData($bridgedRequest);
+		
+		return Response::json(array(
+			'private' => 'stuff',
+			'user_id' => $token['user_id'],
+			'client'  => $token['client_id'],
+			'expires' => $token['expires'],
+		));
+	}
+	else {
+		return Response::json(array(
+			'error' => 'Unauthorized'
+		), $bridgedResponse->getStatusCode());
+	}
+});
