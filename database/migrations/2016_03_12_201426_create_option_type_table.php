8a9ee03fab385aa287247e5ae581fe51ca42021d
Javier Lopez <javierlopm@hotmail.com>
2016-03-12 15:48:27 -0430

    Archivos faltantes

---
+++ database/migrations/2016_03_12_201426_create_option_type_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOptionTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('option_types', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('field_descriptor_id')->unsigned();
+            $table->foreign('field_descriptor_id')->references('id')->on('field_descriptors');
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('option_types');
+    }
+}
